6a7c4985cec936f2f0a7dab957f7a9ea104d2f2c
TJ1451862 <gcr1995@126.com>
2021-02-27 20:46:15 +0800

    增添了读数据的工具类

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-import csv
+import utils
 
 data_path = 'E:\\code\\smart_cite_con\\data\\bertData1_20201203.csv'
 
@@ -15,9 +15,8 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    f = csv.reader(open(data_path, "r", encoding='utf-8'))
-    for i in f:
-        print(i)
+    data_list = utils.read_data(data_path)
+    print(data_list)
     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
--- utils.py
+++ utils.py
@@ -1,1 +1,15 @@
 import csv
+
+
+def read_data(data_path):
+    f = csv.reader(open(data_path, "r", encoding='utf-8'))
+    data_list = []
+    is_first_line = True
+    for i in f:
+        if is_first_line:
+            is_first_line = False
+            continue
+        else:
+            i[2] = int(i[2])
+            data_list.append(i)
+    return data_list
